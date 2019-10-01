70e08d69db2236eed0bb82b4d7fadf9acc7d0fcf
Qwest <tua_qwest@mail.ru>
2019-10-01 23:53:55 +0300

    Первая версия (рисуется флагшток)

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((600, 600))
+
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    pygame.draw.rect(screen, (197, 201, 199), [60, 50, 10, 500], 0)
+
+    pygame.display.flip()
+
+pygame.quit()
